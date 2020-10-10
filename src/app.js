cebf1eeca1174591b7066073618105dff2988dc6
강수성 <mercuriver@gmail.com>
2020-10-10 20:29:22 +0900

    Lint-prettier quiz

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
+/**
+ * TODO: Prettier가 스타일을 수정합니다.
+ */
+
+
 import MainController from './controllers/MainController.js'
+
 import './app.scss'
+
 
 document.addEventListener("DOMContentLoaded", ()=>{
 new MainController(); 
