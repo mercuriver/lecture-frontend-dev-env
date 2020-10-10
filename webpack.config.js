8a5f540c77c6667ec0a68e0eb5ae61e07c83d1d3
강수성 <mercuriver@gmail.com>
2020-10-10 14:48:59 +0900

    Babel-babel quiz

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
 const path = require("path");
 const webpack = require("webpack");
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 module.exports = {
   mode: "development",
@@ -16,11 +19,11 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/,
+        test: /\.(scss|css)$/,
         use: [
-          process.env.NODE_ENV === 'production' 
-          ? MiniCssExtractPlugin.loader
-          : "style-loader", 
+          process.env.NODE_ENV === "production"
+            ? MiniCssExtractPlugin.loader // 프로덕션 환경
+            : "style-loader", // 개발 환경
           "css-loader"
         ]
       },
@@ -29,26 +32,35 @@ module.exports = {
         loader: "url-loader",
         options: {
           name: "[name].[ext]?[hash]",
-          limit: 20000 
+          limit: 10000 // 10Kb
         }
       }
+      /**
+       * TODO: babel-loader를 구성해 보세요.
+       */
     ]
   },
   plugins: [
     new webpack.BannerPlugin({
-      banner: `Build Date: ${new Date().toLocaleString()}`
+      banner: `빌드 날짜: ${new Date().toLocaleString()}`
     }),
     new HtmlWebpackPlugin({
-      template: './src/index.html',
+      template: "./src/index.html",
       templateParameters: {
-        env: process.env.NODE_ENV === 'production' ? '':'(개발용)'
-      }
+        env: process.env.NODE_ENV === "development" ? "(개발용)" : ""
+      },
+      minify:
+        process.env.NODE_ENV === "production"
+          ? {
+              collapseWhitespace: true, // 빈칸 제거
+              removeComments: true // 주석 제거
+            }
+          : false,
+      hash: process.env.NODE_ENV === "production"
     }),
-    new CleanWebpackPlugin({}),
-    ...(
-      process.env.NODE_ENV === 'production' 
-      ? [new MiniCssExtractPlugin({filename: '[name].css'})]
-      : []
-    )
+    new CleanWebpackPlugin(),
+    ...(process.env.NODE_ENV === "production"
+      ? [new MiniCssExtractPlugin({ filename: `[name].css` })]
+      : [])
   ]
 };
