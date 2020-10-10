13371e5e92be4f9ead6c23bb2ac81ab4962b4866
강수성 <mercuriver@gmail.com>
2020-10-10 20:39:42 +0900

    Lint-prettier solution

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,18 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es6": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "plugin:prettier/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
     "parserOptions": {
-        "ecmaVersion": 12,
+        "ecmaVersion": 2018,
         "sourceType": "module"
     },
     "rules": {
