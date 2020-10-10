bbd2685b87215fc2cbdfadd00b2508c233b59c52
강수성 <mercuriver@gmail.com>
2020-10-10 12:13:10 +0900

    webpack-plugin solution

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,8 @@
 const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   mode: "development",
@@ -13,23 +17,38 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: [
+          process.env.NODE_ENV === 'production' 
+          ? MiniCssExtractPlugin.loader
+          : "style-loader", 
+          "css-loader"
+        ]
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
         loader: "url-loader",
         options: {
           name: "[name].[ext]?[hash]",
-          limit: 10000 // 10Kb
+          limit: 20000 
         }
       }
     ]
-  }
-  /**
-   * TODO: 아래 플러그인을 추가해서 번들 결과를 만들어 보세요.
-   * 1. BannerPlugin: 결과물에 빌드 시간을 출력하세요.
-   * 2. HtmlWebpackPlugin: 동적으로 html 파일을 생성하세요.
-   * 3. CleanWebpackPlugin: 빌드 전에 아웃풋 폴더를 깨끗히 정리하세요.
-   * 4. MiniCssExtractPlugin: 모듈에서 css 파일을 분리하세요.
-   */
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: `Build Date: ${new Date().toLocaleString()}`
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      templateParameters: {
+        env: process.env.NODE_ENV === 'production' ? '':'(개발용)'
+      }
+    }),
+    new CleanWebpackPlugin({}),
+    ...(
+      process.env.NODE_ENV === 'production' 
+      ? [new MiniCssExtractPlugin({filename: '[name].css'})]
+      : []
+    )
+  ]
 };
