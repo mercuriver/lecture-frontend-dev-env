05beeb982a2761e7b8866e89d7bf5a1740b5c452
강수성 <mercuriver@gmail.com>
2020-10-10 20:55:28 +0900

    Husky hook test 4

--- src/app.js
+++ src/app.js
@@ -1,9 +1,4 @@
-/**
- * TODO: Prettier가 스타일을 수정합니다.
- */
-
 import MainController from "./controllers/MainController.js";
-
 import "./app.scss";
 
 document.addEventListener("DOMContentLoaded", () => {
